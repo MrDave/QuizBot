d79f361590e623d66e073741f0d4684f097dd3c3
David Schnoll <davidschnoll@gmail.com>
2024-02-23 16:29:10 +0200

    Implement user keyboard; send new question

--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,8 @@
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CallbackContext, MessageHandler, CommandHandler, Filters
 from environs import Env
+from random import choice
+from quiz_handlers import dictify_questions
 
 
 def echo(update: Update, context: CallbackContext):
@@ -10,10 +12,21 @@ def echo(update: Update, context: CallbackContext):
 
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
+    keyboard = [
+        ["Новый вопрос", "Сдаться"],
+        ["Мой счёт"]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     update.message.reply_markdown_v2(
         fr"Hola {user.mention_markdown_v2()}\! I'm your bot for today ❤️",
-        reply_markup=ForceReply(selective=True)
+        reply_markup=reply_markup
     )
+
+
+def send_question(update: Update, context: CallbackContext):
+    questionnaire = context.bot_data["questionnaire"]
+    question, answer = choice(questionnaire)
+    update.message.reply_text(question)
 
 
 def main():
@@ -24,8 +37,13 @@ def main():
 
     updater = Updater(token=bot_token)
     dp = updater.dispatcher
+    with open("quiz_questions/pliga51.txt", encoding="KOI8-R") as file:
+        text = file.read()
+    dp.bot_data["questionnaire"] = dictify_questions(text)
+    dp.update_persistence()
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.regex(r"^Новый вопрос$"), send_question))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     updater.start_polling()
